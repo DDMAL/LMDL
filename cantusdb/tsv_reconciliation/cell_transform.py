b803f17ca6d081c7e0b36380e454eb262628913b
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-06-13 12:52:10 -0400

    Create cell_transform.py

---
+++ cantusdb/tsv_reconciliation/cell_transform.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import sys
+import csv
+from bs4 import BeautifulSoup
+
+tsv_filename = os.path.join(os.path.dirname(__file__), sys.argv[1])
+
+def get_html_title(url):
+    # Send a request to the URL
+    response = requests.get(url)
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Find the title tag
+    title = soup.title.string
+    return title
+
+transforming_column = ['feast_id','genre_id','office_id','source_id']
+
+
