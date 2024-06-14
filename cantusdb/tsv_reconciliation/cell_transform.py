a0a342010d1f1366aacd473c15a6e1e923395715
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-06-14 10:35:28 -0400

    feat: use pandas to transform the raw TSV

--- cantusdb/tsv_reconciliation/cell_transform.py
+++ cantusdb/tsv_reconciliation/cell_transform.py
@@ -1,23 +1,26 @@
 import requests
 import os
 import sys
-import csv
+import pandas as pd
 from bs4 import BeautifulSoup
 
 tsv_filename = os.path.join(os.path.dirname(__file__), sys.argv[1])
 
 def get_html_title(url):
-    # Send a request to the URL
-    response = requests.get(url)
-    # Parse the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Find the title tag
-    title = soup.title.string
-    return title
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.title.string if soup.title else 'No Title Found'
+    except requests.RequestException as e:
+        return f'Error: {e}'
 
-transforming_column = ['feast_id','genre_id','office_id','source_id']
+transforming_column = ['feast','genre','office','source']
 
-with open(tsv_filename) as fd:
-    tsv_reader = csv.reader(fd, delimiter='\t', quotechar='"')
-    for row in tsv_reader:
-        
+df = pd.read_csv(tsv_filename, sep='\t')
+df.drop('json_info', axis=1, inplace=True)
+for column in transforming_column:
+    df[column+'_id'] = f"https://cantusdatabase.org/{column}/" + df[column+'_id'].astype(str)
+    df[column+'_name'] = df[column+'_id'].apply(get_html_title)
+
+df.to_csv('cantus_transformed.tsv', sep='\t', index=False)
